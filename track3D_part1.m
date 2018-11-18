6b1b878e988680efd9699ff0d0f0c13c78aa681f
unknown <goncalorrc@gmail.com>
2018-11-18 21:45:16 +0000

    la

--- track3D_part1.m
+++ track3D_part1.m
@@ -9,9 +9,9 @@ nr_frames = size(imgseq1.rgb,3);
 
 bg_depth = median(imgseq1.depth,3);
 bg_gray = median(imgseq1.rgb,3);
-%figure(1);
-%subplot(211);imagesc(bgdepth);
-%subplot(212);imagesc(bggray);
+% figure(10);
+% subplot(211);imagesc(bg_depth);
+% subplot(212);imagesc(bg_gray);
 
 % figure(1);clf;
 % figure(2);clf;
@@ -29,21 +29,22 @@ bg_gray = median(imgseq1.rgb,3);
      connected_components = bwlabel(imgdiffiltered(:,:,i));
      s = regionprops(connected_components, 'centroid');
      a = regionprops(connected_components, 'area');
-     figure(i);
-     imagesc(bwlabel(imgdiffiltered(:,:,i)));
-     title('Connected components');
+      
+%      figure(i);
+%      imagesc(bwlabel(imgdiffiltered(:,:,i)));
+%      title('Connected components');
 
-     if (i == 5)
-     figure(i)
-     imagesc(bwlabel(imgdiffiltered(:,:,i)));
-     title('Connected components');
-     end
-     
-     if (i == 4)
-     figure(i)
-     imagesc(bwlabel(imgdiffiltered(:,:,i)));
-     title('Connected components - previous');
-     end
+%      if (i == 5)
+%      figure(i)
+%      imagesc(bwlabel(imgdiffiltered(:,:,i)));
+%      title('Connected components');
+%      end
+%      
+%      if (i == 4)
+%      figure(i)
+%      imagesc(bwlabel(imgdiffiltered(:,:,i)));
+%      title('Connected components - previous');
+%      end
      
      
      for k=1:size(s,1) % for every region 
@@ -61,12 +62,12 @@ bg_gray = median(imgseq1.rgb,3);
      measurements(i).centroids = [centroidX' centroidY'];
      measurements(i).ids = id;
      
-     %if(i == 5 || i == 4)
+     if(i == 5 || i == 4)
         for k=1:size(s,1)
             text(centroidX(k), centroidY(k), num2str(k), 'FontSize', 14, 'FontWeight', 'Bold'); 
             measurements(i).centroids(k,3) = k;
         end
-     %end
+     end
      %hold on
      %plot(centroids(:,1), centroids(:, 2), 'b*');
      %hold off
@@ -83,8 +84,9 @@ bg_gray = median(imgseq1.rgb,3);
             for b=1:size(measurements(i-1).centroids,1)
              X = [measurements(i).centroids(a, 1),measurements(i).centroids(a, 2); measurements(i-1).centroids(b, 1), measurements(i-1).centroids(b, 2)];
              distances(a, b, i) = pdist(X, 'euclidean');
-             areas(a, b, i) = measurements(i).areas(a) - measurements(i-1).areas(b);
-             cost(a, b, i) = distances(a, b, i);% - areas(a, b, i);
+%             abs(measurements(i).areas(a) - measurements(i-1).areas(b))
+             areas(a, b, i) = abs(measurements(i).areas(a)/measurements(i-1).areas(b));
+             %cost(a, b, i) = distances(a, b, i);% - areas(a, b, i);
              %if(distances(a, b) > 60)
               %   distances(a, b) = NaN;
              %end
@@ -98,39 +100,57 @@ bg_gray = median(imgseq1.rgb,3);
  end
  for i=1:size(distances, 3)
      for a=1:size(distances, 2)
-         for b=1:size(distances, 1)
-             if(distances(a, b, i) < 10)
-                 distances(a, b, i) = NaN;
-             end
+         for b=1:size(distances, 1)            
+
+                 distances(a, b, i) = distances(a, b, i) / 50;
+              cost_function(a, b, i) = abs(areas(a, b, i) - 1)*0.75 + abs(distances(a, b, i) - 1)*0.25; 
          end
+
      end
+      cost_function(:,:,i) = cost_function(:,:,i).';
  end
- 
+
  assignin('base', 'distances', distances);
  assignin('base', 'areas', areas);
  assignin('base', 'id', id);
  assignin('base','measurements', measurements);
- %measurements(4).areas(1)
- %%check for movement
- for i=4:nr_frames
-     if(i == 4)
-      index = 1;
-     end
-     [assignment, cost] = HungarianMethod(distances(:, :, i));
-     
-     object(i) = measurements(i).ids(:, find(assignment == index));
-     index = object(i)
+ assignin('base','cost_function', cost_function);
 
-%    
-%      areas = measurements(i).areas(find(measurements(i).centroids(:, 3) == index))
-     %imageobj = measurements(i).areas(:, find(measurements(i-1).centroids(:,3) == index))
-     %index = 
-%      if(size(object,1) > 1) %choose the one with the closest area
-%          [~, index] = min(abs(imageobj - measurements(i).areas(1)))
-%      else
-%          index = object;
-%      end    
+ 
+ %% check for movement
+ 
+ for i=4:nr_frames
+%       if(i == 4)
+%        index = 1;
+%       end
+     [assignment, cost] = HungarianMethod(cost_function(:, :, i));
+    % index = assignment(index);
+        %i
+    for a=1:length(assignment)
+        costs = cost_function(a,assignment(a),i);
+        if(costs > 0.6)
+            objects(a, i) = false;
+        else
+            objects(a, i) = assignment(a);
+        end
+        
+    end
+          
  end
+ 
+
+  assignin('base','objects', objects);
+  
+  for a = 4:size(objects, 2)
+    
+      for b = 5:size(objects, 1)
+          moving_objects(a, b) = ;
+      end
+      
+  end
+     
+   assignin('base','moving_objects', moving_objects);  
+
 
 end
 
