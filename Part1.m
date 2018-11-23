1e6624dbb4f7ac9182801fbed8dc95e819c4de75
unknown <goncalorrc@gmail.com>
2018-11-23 12:16:28 +0000

    atualizado

--- Part1.m
+++ Part1.m
@@ -1,17 +1,20 @@
 close all;
 clear;
 load('cameraparametersAsus.mat');
-load('depth_10.mat');
+%load('depth_10.mat');
 
-d=dir('*.jpg');
-dd=dir('*.mat');
+D = 'imgseq1';
+%d=dir(fullfile(D,'*.jpg'));
+%dd=dir(fullfile(D,'*.mat'));
+d = dir('images*.jpg');
+dd = dir('images*.mat');
 
 a = zeros(480,640,length(d));
 b = zeros(480, 640, length(d));
 for i=1:length(d)    
     a(:,:,i) = rgb2gray(imread(d(i).name));
-    load(dd(i).name);
-    b(:,:,i) = double(depth_array)/1000;
+    name = load(dd(i).name);
+    b(:,:,i) = double(name.depth_array)/1000;
     %figure(1)
     %imshow(uint8(imgseq1(:,:,i).rgb));
     %figure(2);
--- track3D_part1.m
+++ track3D_part1.m
@@ -17,7 +17,7 @@ bg_gray = median(imgseq1.rgb,3);
 % figure(2);clf;
  for i=1:nr_frames
      imdiff = imgseq1.depth(:,:,i) > 0 & abs(imgseq1.depth(:,:,i) - bg_depth)>.20;
-     imgdiffiltered(:,:,i) = imopen(imdiff,strel('disk',5));
+     imgdiffiltered(:,:,i) = imopen(imdiff,strel('disk',6));
      %figure(1);
     % imagesc([imdiff imgdiffiltered]);
      %title('Difference image and morph filtered');
@@ -30,9 +30,10 @@ bg_gray = median(imgseq1.rgb,3);
      s = regionprops(connected_components, 'centroid');
      a = regionprops(connected_components, 'area');
       
-%      figure(i);
-%      imagesc(bwlabel(imgdiffiltered(:,:,i)));
-%      title('Connected components');
+     figure(i);
+     imagesc(bwlabel(imgdiffiltered(:,:,i)));
+     title('Connected components');
+
 
 %      if (i == 5)
 %      figure(i)
@@ -62,12 +63,13 @@ bg_gray = median(imgseq1.rgb,3);
      measurements(i).centroids = [centroidX' centroidY'];
      measurements(i).ids = id;
      
-     if(i == 5 || i == 4)
+     %if(i == 5 || i == 4)
         for k=1:size(s,1)
             text(centroidX(k), centroidY(k), num2str(k), 'FontSize', 14, 'FontWeight', 'Bold'); 
             measurements(i).centroids(k,3) = k;
         end
-     end
+        %pause;
+     %end
      %hold on
      %plot(centroids(:,1), centroids(:, 2), 'b*');
      %hold off
@@ -76,7 +78,7 @@ bg_gray = median(imgseq1.rgb,3);
      
  end
  
- for i=4:nr_frames
+ for i=2:nr_frames
       %  current = imgdiffiltered(i);
        % previous = imgdiffiltered(i-1);
         %size(measurements(6).centroids,1);
@@ -85,13 +87,13 @@ bg_gray = median(imgseq1.rgb,3);
              X = [measurements(i).centroids(a, 1),measurements(i).centroids(a, 2); measurements(i-1).centroids(b, 1), measurements(i-1).centroids(b, 2)];
              distances(a, b, i) = pdist(X, 'euclidean');
 %             abs(measurements(i).areas(a) - measurements(i-1).areas(b))
-             areas(a, b, i) = abs(measurements(i).areas(a)/measurements(i-1).areas(b));
-             %cost(a, b, i) = distances(a, b, i);% - areas(a, b, i);
-             %if(distances(a, b) > 60)
-              %   distances(a, b) = NaN;
-             %end
+               if(measurements(i).areas(a) > 400 && measurements(i-1).areas(b) > 400)
+                areas(a, b, i) = abs(measurements(i).areas(a)/measurements(i-1).areas(b));
+               else
+                   areas(a, b, i) = NaN;
+               end
             end
-            [value, arg] = min(distances(a, :, 4));
+            [value, arg] = min(distances(a, :, i));
             id(a) = arg;%returns the index of the closest centroid of the previous frame
             measurements(i).centroids(a,3) = id(a);
             
@@ -101,8 +103,11 @@ bg_gray = median(imgseq1.rgb,3);
  for i=1:size(distances, 3)
      for a=1:size(distances, 2)
          for b=1:size(distances, 1)            
-
-                 distances(a, b, i) = distances(a, b, i) / 50;
+              if(areas(a, b, i) == 0)
+                  areas(a, b, i) = NaN;
+              end
+              
+              distances(a, b, i) = distances(a, b, i) / 50;
               cost_function(a, b, i) = abs(areas(a, b, i) - 1)*0.75 + abs(distances(a, b, i) - 1)*0.25; 
          end
 
@@ -119,17 +124,19 @@ bg_gray = median(imgseq1.rgb,3);
  
  %% check for movement
  
- for i=4:nr_frames
+ for i=2:nr_frames
 
      [assignment, cost] = HungarianMethod(cost_function(:, :, i));
     % index = assignment(index);
     
     for a=1:length(assignment)
-        costs = cost_function(a,assignment(a),i);
-        if(costs > 0.6)
-            objects(a, i) = false;
-        else
-            objects(a, i) = assignment(a);
+        if(assignment(a)~=0)
+            costs = cost_function(a,assignment(a),i);
+            if(costs > 3)
+                objects(a, i) = false;
+            else
+                objects(a, i) = assignment(a);
+            end
         end
         
     end
@@ -138,22 +145,101 @@ bg_gray = median(imgseq1.rgb,3);
  
 
   assignin('base','objects', objects);
-  
-  for a = 1:size(objects, 1)
-    moving_objects(a, 4) = objects(a, 4);
-    x = moving_objects(a,4);
-      for b = 5:size(objects, 2);
-          x = objects(x, b - 1)
-          if(x == 0)              
-            continue;
-          end
-          moving_objects(a, b) = objects(x, b);
-          
+  moving_objects = zeros(size(objects, 1),size(objects, 2)); 
+  linhas = zeros(size(moving_objects,1),1);
+  for b = 1:size(objects, 2) % colunas
+      for a = 1:size(objects, 1) % linhas
+              if(objects(a, b) ~= 0) % if an object is found
+                  while_finished = false;
+                  b
+                  x = objects(a, b)
+                  %objects(:, b - 1)';
+                  %I want to find if, in the matrix objects, the line of
+                  %the current object is an entry from the last column
+                  %the line is given by find(objects(:, b) == x);
+                  if(find(objects(:, b - 1) == a)) %check if there is a previous object
+                      z = b - 1; %column where that previous object is
+                     while not(while_finished) % while we haven't found the "first previous"                    
+                          for j = 1:size(objects,1) %go through every line of the matrix
+                             if(z == b - 1) %for finding the "most recent previous"
+                                 if(objects(j, z) == a) %if there is an object that has the value of line of the next object
+                                     previous_object = objects(j, z)
+                                     break;
+                                 end
+                             else
+                                %previous_object
+                                 find(objects(:, z + 1) == previous_object);
+                                 linez = find(objects(:, z + 1) == previous_object)
+                                 if(moving_objects(j, z) == linez)
+                                     previous_object = moving_objects(j, z)
+                                     break;
+                                 else
+                                     while_finished = true;
+                                     break;
+                                 end
+                             
+                                 
+                             end
+                          end
+                          z
+%                           if( not(isempty(find(linhas(:) == previous_object))) && z == frames(find(linhas(:) == previous_object)))
+%                               previous_object;
+%                               linhas(previous_object);
+%                               moving_objects(find(linhas(:,1) == previous_object), b) = x;
+%                               break;
+%                           end
+                         % if(previous
+                          z = z - 1;
+                          if(z == 0)                             
+                              break;
+                              while_finished = true;
+                          end
+                     end
+                          %só quero que corra isto quando chegar ao "first
+                          %previous"
+                          %previous_object
+                     if(while_finished)
+                          previous_object;
+                          linhas(previous_object);
+                          moving_objects(find(linhas(:,1) == previous_object), b) = x;
+                     end
+
+                      %object_line = objects(:, b - 1) == find(objects(:, b) == x);
+                      for j=1:size(objects, 1)                          
+                          if(objects(j, b - 1) == a)
+                              y = objects(j, b - 1);
+                              %moving_objects(a, b) = objects(a,b);
+                          end 
+                      end
+                  else
+                      for y = 1:size(moving_objects,1)
+                         if(moving_objects(y,:) == 0)
+                             lineeee = y;
+                             break;
+                         end
+                      end
+                       %line = find(moving_objects() == 0,1);
+                       x = objects(a, b);
+                       if(objects(x, b + 1) ~= 0)
+                           moving_objects(lineeee, b) = objects(a, b);
+                           linhas(lineeee) = objects(a, b);
+                           frames(lineeee) = b;
+                       end
+                  end
+
+              end
+
+         
       end
-      
+                      
   end
-     
+   %assignin('base','frames_first_object', frames_first_object); 
+   assignin('base', 'frames', frames);
+   assignin('base', 'linhas', linhas);
    assignin('base','moving_objects', moving_objects);  
+
+
+
 
 
 end
